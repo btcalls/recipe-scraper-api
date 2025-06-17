6463e157f3d270aedd065229efb3e14d3e2c76f3
Jason Jon Carreos <jasondev06@gmail.com>
2025-06-17 21:06:41 +1000

    More verbose parsing error message.

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ async def parse_url(url: str = Body(..., embed=True)):
 
     if recipe is None:
         raise HTTPException(
-            status_code=500,
-            detail=f"Failed to parse the recipe at URL: {url}."
+            status_code=501,
+            detail=f"Failed to parse the recipe at {url}. Please check the URL or try a different one."
         )
 
     return recipe.to_dict()
